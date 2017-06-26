69c9300888a709699d197123bc8f5deb9eae5ff1
Marc Letouzé <marc.letouze@liubov.net>
2017-06-26 17:59:25 +0200

    Add files via upload

--- site/helpers/category.php
+++ site/helpers/category.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version  V2.0
+ * @version    CVS: 2.1.1
  * @package    Com_Manifest2md
  * @author     Emmanuel Lecoester <elecoest@gmail.com>
  * @author     Marc Letouzé <marc.letouze@liubov.net>
